7642d2e888903a3b28829a23f517deef71f5b8df
EytanGF <egoldhaberfiebert2025@kehillahstudent.org>
2022-02-28 19:52:11 -0800

    complete register functions

--- funcs.py
+++ funcs.py
@@ -1,11 +1,10 @@
 
-def open_file_w(filename):
-    myFile = open(filename, 'w')
+def open_file(filename, mode):
+    myFile = open(filename, mode)
     return myFile
 
-def open_file_r(filename):
-    myFile = open(filename, 'r')
-    return myFile
+def close_file(filename):
+    close(filename)
 
 def startmenu():
     print("Hi, welcome to user management")
@@ -14,27 +13,72 @@ def startmenu():
     resp = input()
     return resp
 
+def check_username(entered_username):
+    usernames = open_file('usernames.txt', 'r')
+    for line in usernames:
+        if entered_username == line.strip():
+            print("Sorry, that username is already taken. Please enter a different username")
+            return False
+        else:
+            return True
+
+def username_create():
+    usernames = open_file('usernames.txt', 'r')
+    print("Please enter a username")
+    username = input()
+    status = False
+    while status == False:
+        status = check_username(username)
+        if status == False:
+            username = input()
+        else:
+            status = True
+    return username
+
+def password_create():
+    print("Please enter a password")
+    password = input()
+    while len(password) < 8:
+        print("Sorry, you password needs to be at least 8 characters. Please reenter a password.")
+        password = input()
+    return password
+
+def after_register():
+    print("You are now registered. Would you like to 1) losgin to your account or 2) exit")
+    resp = input()
+    return resp
+
+def login_true_msg():
+        if login() == True:
+            print("You are now logged in. Welcome")
+
 
 def main():
     resp = startmenu()
-    register()
     if int(resp) == 1:
-        register()
+        if register() == True:
+            resp = after_register()
+            if int(resp) == 1:
+                login_true_msg()
+            elif int(resp) == 2:
+                exit()
     if int(resp) == 2:
-        login()
+        login_true_msg()
+
 
 def register():
-    db = open_file_w('userdata.txt')
-    print("Please enter a username")
-    username = input()
-    print("Please enter a password")
-    password = input()
-    db.write('{0},{1}'.format(username, password))
+    usernames = open_file('usernames.txt', 'a')
+    passwords = open_file('passwords.txt', 'a')
+    username = username_create()
+    password = password_create()
+    usernames.write('{0}'.format(username + '\n'))
+    passwords.write('{0}'.format(password + '\n'))
+    return True
 
 def login():
     print("Please enter a username, case sensitive")
     username = input()
-    db = open_file_r('userdata.txt')
-    for line in db:
-        if line == username:
-            return True
+    usernames = open_file('usernames.txt', 'r')
+    # for line in usernames:
+    #     if username == line:
+    #         ""
